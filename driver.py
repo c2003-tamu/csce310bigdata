eb85749aa3d95f2c6ee3e8f3f7c37effeff94a68
gmgrahamgm <grahamd@tamu.edu>
2024-11-23 16:05:23 -0600

    Added comments

--- driver.py
+++ driver.py
@@ -2,12 +2,22 @@ import re
 from datetime import datetime
 import math
 
+"""
+Takes parsed lines and creates key, value pairs based on the three analyses that will be performed.
+"""
+
 
 def map(lines):
     dict_overall = {}
     for i in range(len(lines)):
         dict_overall[lines[i][0], lines[i][1], lines[i][4]] = lines[i]
     return dict_overall
+
+
+"""
+Takes a raw input line and separates all values based on regular expressions.
+Error handling is implemented via regex returning None values if a line does not meet the format.
+"""
 
 
 def parse(log_line):
@@ -33,18 +43,19 @@ def parse(log_line):
         return None
 
 
+"""
+Takes a mapped dictionary of key, value pairs and performs analysis on the data via reduction.
+IP's are marked as suspicious if they attempt to access '/admin' or '/config'.
+IPs are also marked as suspicious if they attempt to access 401 or 403 more than ten times.
+"""
+
+
 def reduce(map_dict):
     sus_list = ["/admin", "/config"]
 
     # IP ANALYSIS --------------------------------------------------
     ip_analysis_dict = {}
     for ip_entry in map_dict:
-        # num_instances
-        # num_success
-        # count_401
-        # count_403
-        # is_sus
-
         # Doesn't exist
         if ip_analysis_dict.get(ip_entry[0]) is None:
             ip_analysis_dict[ip_entry[0]] = {
@@ -134,6 +145,14 @@ def reduce(map_dict):
     return [ip_analysis_dict, time_window_dict, error_analysis_dict]
 
 
+"""
+A driver function for the MapReduce file.
+Opens a file in the same directory called 'updated_network.log' and calls the parser on the raw data.
+The parsed data is then mapped, creating a dictionary of key,value pairs
+The mapped key,values are then reduced, and their output data (in the form of dictionaries) are printed.
+"""
+
+
 def driver():
     log_file = './updated_network.log'
     with open(log_file, 'r') as file:
@@ -147,10 +166,7 @@ def driver():
             parsed_lines.append(parsed_line)
 
     map_dict = map(parsed_lines)
-    reduced_ips = {}
-    reduced_times = {}
-    reduced_requests = {}
-    [reduced_ips, reduced_times, reduced_request] = reduce(map_dict)
+    [reduced_ips, reduced_times, reduced_requests] = reduce(map_dict)
 
     # IP Analysis
     print("--- IP Analysis ---")
@@ -200,7 +216,7 @@ def driver():
             peak = reduced_times[reduced_time]
             peak_time = reduced_time
 
-    print("--- Hourly Analysis ---")
+    print("\n--- Hourly Analysis ---")
     for reduced_time in reduced_times:
         time_str = f"{
             reduced_time}-{hours_dict[reduced_time]}: {reduced_times[reduced_time]} requests"
@@ -209,17 +225,17 @@ def driver():
         print(time_str)
 
     # Error Request Analysis
-    print("--- Error Analysis ---")
-    for error_request in reduced_request:
+    print("\n--- Error Analysis ---")
+    for error_request in reduced_requests:
         # Get the top url
         top_url_dict = {"top_url": "", 'top_frequency': -1}
-        for url in reduced_request[error_request]['url_frequency']:
-            if reduced_request[error_request]['url_frequency'][url] > top_url_dict['top_frequency']:
+        for url in reduced_requests[error_request]['url_frequency']:
+            if reduced_requests[error_request]['url_frequency'][url] > top_url_dict['top_frequency']:
                 top_url_dict["top_url"] = url
-                top_url_dict["top_frequency"] = reduced_request[error_request]['url_frequency'][url]
+                top_url_dict["top_frequency"] = reduced_requests[error_request]['url_frequency'][url]
 
         print(f"{error_request}: {
-              reduced_request[error_request]['num_instances']} occurrences (top URL: {top_url_dict["top_url"]})")
+              reduced_requests[error_request]['num_instances']} occurrences (top URL: {top_url_dict["top_url"]})")
 
 
 if __name__ == "__main__":
